dc360107355c1779db20c00c1c82fcf40a4b021e
adarshshukla5658172@gmail.com <adarshshukla5658172@gmail.com>
2023-10-30 07:52:37 +0530

    Free fire

---
+++ main.js
@@ -0,0 +1,37 @@
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Game Homepage</title>
+  <style>
+    body {
+      background-image: url(background.jpg);
+      background-size: cover;
+    }
+  </style>
+</head>
+<body>
+  <h1>Welcome to the Game Homepage!</h1>
+  <h2>Popular Games:</h2>
+  <ul>
+    <li><a href="free_fire.html">Free Fire</a></li>
+    <li><a href="clash_of_clans.html">Clash of Clans</a></li>
+    <li><a href="call_of_duty.html">Call of Duty</a></li>
+  </ul>
+  <img src="image1.jpg" alt="Game Image 1">
+  <img src="image2.jpg" alt="Game Image 2">
+  <h2>Free Fire</h2>
+  <p>Free Fire is an action-packed battle royale game where you fight to be the last one standing!</p>
+  <img src="free_fire_gif.gif" alt="Free Fire GIF">
+  <img src="free_fire_image.jpg" alt="Free Fire Image">
+  <h2>Clash of Clans</h2>
+  <p>Clash of Clans is a strategy game where you build your village, train troops, and battle against other players!</p>
+  <img src="clash_of_clans_gif.gif" alt="Clash of Clans GIF">
+  <img src="clash_of_clans_image.jpg" alt="Clash of Clans Image">
+  <h2>Call of Duty</h2>
+  <p>Call of Duty is a first-person shooter game that takes you into intense military combat missions!</p>
+  <img src="call_of_duty_gif.gif" alt="Call of Duty GIF">
+  <img src="call_of_duty_image.jpg" alt="Call of Duty Image">
+</body>
+</html>
+`
